bc51dbdfd486feaf7bf9965645372d5052ae7b94
DiogoLeonardoGit <63851213+DiogoLeonardoGit@users.noreply.github.com>
2020-05-21 12:41:06 +0100

    Add files via upload

---
+++ Tarefa 7/testeSQL/Form2.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form2
+    Dim c As Integer = 0
+
+    Private Sub btnLogin_Click(sender As Object, e As EventArgs)
+
+        Dim f As New Form3
+        f.Show()
+    End Sub
+
+    Private Sub waitaminute_Tick(sender As Object, e As EventArgs) Handles waitaminute.Tick
+        c += 1
+        If c = 1 Then
+            Me.Hide()
+            Form3.Show()
+        End If
+    End Sub
+End Class
